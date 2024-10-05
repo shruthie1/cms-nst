bb46d9bf9bda715424e0a53c45ebfe166562db38
SSK <dodieajt@gmail.com>
2024-10-05 18:24:17 +0530

    fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BufferClientModule, ChannelsModule, ClientModule, BuildModule } from 'commonService';
+import { BufferClientModule, ChannelsModule, ClientModule, BuildModule, LoggerMiddleware } from 'commonService';
+import { MiddlewareConsumer } from '@nestjs/common';
 
 @Module({
   imports: [BuildModule, ClientModule, BufferClientModule, ChannelsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
